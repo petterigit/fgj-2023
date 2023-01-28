ce746dc1c318940cfed491b6249407017417f0be
Vilperi <p-makela@outlook.com>
2023-01-28 17:48:25 +0200

    Comment consts

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,7 @@
+/**
+ * Constants that are required in both the game & the site
+ */
+
 export const gameCanvasID = `game-canvas-${Math.random() * 10000}`;
 export const gameContainerID = `game-container-${Math.random() * 10000}`;
 export const gameStartButtonID = `game-start-button-${Math.random() * 10000}`;
