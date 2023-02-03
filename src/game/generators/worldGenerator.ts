11325d4a012e322b8c034b32506013d87a8e04b3
Niku Grönberg <gronbergniku@gmail.com>
2023-02-03 23:07:01 +0200

    ADd world generations

--- src/game/generators/worldGenerator.ts
+++ src/game/generators/worldGenerator.ts
@@ -1,1 +1,33 @@
-export const generateLevel = () => {};
+import * as noise from 'quick-perlin-noise-js';
+
+interface Rgb {
+    r: number;
+    g: number;
+    b: number;
+}
+
+export const generateLevel = (
+    width: number,
+    height: number,
+    resolution: number,
+    zValue: number
+) => {
+    const map: Rgb[][] = new Array(width).fill(0).map(() => new Array(height));
+
+    for (let y = 0; y < height; ++y) {
+        for (let x = 0; x < width; ++x) {
+            const noiseR =
+                noise.noise(x / resolution, y / resolution, 0 + zValue) + 1;
+            const noiseG =
+                noise.noise(x / resolution, y / resolution, 1 + zValue) + 1;
+            const noiseB =
+                noise.noise(x / resolution, y / resolution, 2 + zValue) + 1;
+            const r = Math.floor(noiseR * 128) & 0xff;
+            const g = Math.floor(noiseG * 128) & 0xff;
+            const b = Math.floor(noiseB * 128) & 0xff;
+            map[x][y] = { r, g, b };
+        }
+    }
+
+    console.log(JSON.stringify(map, null, 2));
+};
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { generateLevel } from 'game/generators/worldGenerator';
 import { initGame } from 'game/main';
 import { createSite } from 'site/site';
 
 createSite();
 initGame();
+generateLevel(10, 10, 2, 4);
