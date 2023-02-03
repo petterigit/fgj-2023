69439c1dfafa0538c0f4da83adf6bdcb53b0cc40
maija <maijaaaw@gmail.com>
2023-02-03 23:53:00 +0200

    type stuff

--- src/game/resources/images/images.ts
+++ src/game/resources/images/images.ts
@@ -9,22 +9,19 @@ import bush1 from 'assets/tilesets/black_and_white_horror/bush1.png';
 /**
  * Type annotations for imported images
  */
-type ImageType = 'duckImage' | 'branch1'  | 'branch2' | 'brick1' | 'bush1';
-
-export type ImageSources = { [I in ImageType]: ImageSource };
 
 /**
  * Create images. Probably same process for other static assets.
  * @returns image files how excalibur wants them
  */
-export const createImages = (): ImageSources => {
-    const images: ImageSources = {
+export const createImages = () => {
+    const images = {
         duckImage: new ImageSource(duckImage),
         branch1: new ImageSource(branch1),
         branch2: new ImageSource(branch2),
         brick1: new ImageSource(brick1),
         bush1: new ImageSource(bush1),
-    };
+    } as const;
 
     return images;
 };
