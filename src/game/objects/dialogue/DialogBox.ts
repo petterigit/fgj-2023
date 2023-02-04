e996a8ab4cc9269a70aa71095bfe32c618e3a877
petterigit <p-makela@outlook.com>
2023-02-04 12:29:01 +0200

    Create somewhat functional dialog

--- src/game/objects/dialogue/DialogBox.ts
+++ src/game/objects/dialogue/DialogBox.ts
@@ -1,10 +1,11 @@
-import { ScreenElement, Vector, Text, Font } from 'excalibur';
+import { ScreenElement, Vector, Text, Font, Color, Sprite } from 'excalibur';
 import { Resources } from 'game/types';
 import { createNextButton } from '../ui-components/NextButton';
 
 export const createDialogBox = (
     dialogs: string[],
     onEnd: () => void,
+    characterSprite: Sprite,
     resources: Resources
 ) => {
     let textIndex = 0;
@@ -12,6 +13,9 @@ export const createDialogBox = (
     const element = new ScreenElement({
         pos: new Vector(400, 800),
         scale: new Vector(1, 1),
+        color: Color.LightGray,
+        width: 1000,
+        height: 1000,
     });
 
     const dialogTexts = dialogs.map(
@@ -22,23 +26,41 @@ export const createDialogBox = (
             })
     );
 
-    element.graphics.use(dialogTexts[0]);
+    /**
+     * SET THESE WHEN YOU CHANGE SPRITE AND NOTHING SHOWS
+     * Made for duck image :] (change the scale with correctly sized sprites)
+     */
+    const characterElement = new ScreenElement({
+        pos: new Vector(470.59, -442.5882352941),
+
+        scale: new Vector(0.5882352941, 0.7352941176),
+    });
+
+    characterElement.graphics.use(characterSprite);
+
+    const dialogTextContainer = new ScreenElement({
+        pos: new Vector(50, 150),
+    });
+
+    dialogTextContainer.graphics.use(dialogTexts[0]);
 
     const nextButton = createNextButton(
         resources,
         () => {
             textIndex++;
             if (dialogTexts.length <= textIndex) {
-                onEnd();
                 // Just in case the dialog box wouldn't be destroyed after done..
+                // No return on purpose to default the text
+                onEnd();
                 textIndex = 0;
-                return;
             }
-            element.graphics.use(dialogTexts[textIndex]);
+            dialogTextContainer.graphics.use(dialogTexts[textIndex]);
         },
-        new Vector(300, 0)
+        new Vector(700, 75)
     );
 
+    element.addChild(characterElement);
+    element.addChild(dialogTextContainer);
     element.addChild(nextButton);
 
     return element;
--- src/game/resources/graphics/graphics.ts
+++ src/game/resources/graphics/graphics.ts
@@ -1,20 +1,17 @@
-import { Graphic, Sprite } from 'excalibur';
+import { Sprite } from 'excalibur';
 import { Resources } from 'game/types';
-
-type GraphicType = 'forward' | 'forwardActive' | 'backward' | 'backwardActive';
-
-export type GraphicSources = { [I in GraphicType]: Graphic };
 
 /**
  * Create images. Probably same process for other static assets.
  * @returns image files how excalibur wants them
  */
-export const createGraphics = (images: Resources['images']): GraphicSources => {
-    const graphics: GraphicSources = {
+export const createGraphics = (images: Resources['images']) => {
+    const graphics = {
         forward: new Sprite({ image: images.forward }),
         forwardActive: new Sprite({ image: images.forwardActive }),
         backward: new Sprite({ image: images.backward }),
         backwardActive: new Sprite({ image: images.backwardActive }),
+        duck: new Sprite({ image: images.duckImage }),
     };
 
     return graphics;
