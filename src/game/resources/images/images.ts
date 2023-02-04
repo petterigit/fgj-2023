4e852391f2323c70543e364dfa1e3b91416d4662
Niku Grönberg <gronbergniku@gmail.com>
2023-02-04 12:06:23 +0200

    Add characters

--- src/game/resources/images/images.ts
+++ src/game/resources/images/images.ts
@@ -6,8 +6,16 @@ import forwardActive from 'assets/images/forward-active.png';
 import backward from 'assets/images/backward.png';
 import backwardActive from 'assets/images/backward-active.png';
 import groundSet from 'assets/tilesets/Forest/Ground Tileset.png';
-
-
+import character1 from 'assets/characters/Fantasy/Character_001.png';
+import character2 from 'assets/characters/Fantasy/Character_002.png';
+import character3 from 'assets/characters/Fantasy/Character_003.png';
+import character4 from 'assets/characters/Fantasy/Character_004.png';
+import character5 from 'assets/characters/Fantasy/Character_005.png';
+import character6 from 'assets/characters/Fantasy/Character_006.png';
+import character7 from 'assets/characters/Fantasy/Character_007.png';
+import character8 from 'assets/characters/Fantasy/Character_008.png';
+import character9 from 'assets/characters/Fantasy/Character_009.png';
+import character10 from 'assets/characters/Fantasy/Character_010.png';
 
 /**
  * Create images. Probably same process for other static assets.
@@ -21,6 +29,16 @@ export const createImages = () => {
         backward: new ImageSource(backward),
         backwardActive: new ImageSource(backwardActive),
         groundSet: new ImageSource(groundSet),
+        characterLavender: new ImageSource(character1),
+        characterShroom: new ImageSource(character2),
+        characterGobbo: new ImageSource(character3),
+        characterFurry: new ImageSource(character4),
+        characterTryhard: new ImageSource(character5),
+        characterBob: new ImageSource(character6),
+        characterAnimeProtagonist: new ImageSource(character7),
+        characterBlondie: new ImageSource(character8),
+        characterBerry: new ImageSource(character9),
+        characterTeacher: new ImageSource(character10),
     } as const;
 
     return images;
--- src/game/spriteSheets/createSpriteSheets.ts
+++ src/game/spriteSheets/createSpriteSheets.ts
@@ -1,8 +1,32 @@
 import { Resources } from 'game/types';
+import { createCharacter } from './characters';
 import { createGround } from './ground';
-
 
 export const createSpriteSheets = (resources: Resources) => {
     const groundSheet = createGround(resources);
-    return { ground: groundSheet };
+    const lavender = createCharacter('Lavender', resources);
+    const shroom = createCharacter('Shroom', resources);
+    const gobbo = createCharacter('Gobbo', resources);
+    const furry = createCharacter('Furry', resources);
+    const tryhard = createCharacter('Tryhard', resources);
+    const bob = createCharacter('Bob', resources);
+    const animeProtagonist = createCharacter('AnimeProtagonist', resources);
+    const blondie = createCharacter('Blondie', resources);
+    const berry = createCharacter('Berry', resources);
+    const teacher = createCharacter('Teacher', resources);
+    return {
+        ground: groundSheet,
+        characters: {
+            lavender,
+            shroom,
+            gobbo,
+            furry,
+            tryhard,
+            bob,
+            animeProtagonist,
+            blondie,
+            berry,
+            teacher,
+        },
+    };
 };
